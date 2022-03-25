ef4a0fb1be64875acedabf9acb4a18441ccdfcc7
uxconor <94135596+uxconor@users.noreply.github.com>
2022-03-24 20:52:38 -0400

    Update scene.js

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -1,7 +1,7 @@
 import {
   Color,
   Scene,
-} from "../../../node_modules/three/build/three.module.js";
+} from "/node_modules/three/build/three.module.js";
 
 function createScene() {
   const scene = new Scene();
