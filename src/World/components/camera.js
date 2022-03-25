69e43880af312a0984fdf999d4bd979d0e12b90a
uxconor <94135596+uxconor@users.noreply.github.com>
2022-03-24 20:51:57 -0400

    Update camera.js

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -1,4 +1,4 @@
-import { PerspectiveCamera } from "../../../node_modules/three/build/three.module.js";
+import { PerspectiveCamera } from "/node_modules/three/build/three.module.js";
 
 function createCamera() {
   const camera = new PerspectiveCamera(35, 1, 0.1, 100);
