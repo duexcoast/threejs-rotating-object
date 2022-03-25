46652bcfc7950aa7247f96a4bff2e188a34063a7
uxconor <94135596+uxconor@users.noreply.github.com>
2022-03-24 20:53:35 -0400

    Update renderer.js

--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -1,4 +1,4 @@
-import { WebGLRenderer } from "../../../node_modules/three/build/three.module.js";
+import { WebGLRenderer } from "/node_modules/three/build/three.module.js";
 
 function createRenderer() {
   const renderer = new WebGLRenderer({ antialias: true });
