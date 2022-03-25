550eabdd70657ce4c1764c715ca5747a7bb3e0b2
uxconor <94135596+uxconor@users.noreply.github.com>
2022-03-24 22:37:44 -0400

    Update controls.js

--- src/World/systems/controls.js
+++ src/World/systems/controls.js
@@ -1,4 +1,4 @@
-import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "../../../node_modules/three/examples/jsm/controls/OrbitControls";
 
 function createControls(camera, canvas) {
   const controls = new OrbitControls(camera, canvas);
